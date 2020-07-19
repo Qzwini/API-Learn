ea7c808dd9dda1bede33a8d2fb5a76bee9757ae0
Qzwini <qzwini@gmail.com>
2020-07-19 18:47:00 +0300

    API GIT

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 print("\n ")
 
-r= requests.get(url='http://127.0.0.1:5000/')
+r= requests.get(url='http://127.0.0.1:5000/me')
 # r= requests.get(url='http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02')
 print("--- status_code --- ")
 print(r)
